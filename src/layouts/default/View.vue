a712530eed6bed8fa7226cd5c7093509368abd8c
GedorN <gedor.silvaneto@gmail.com>
2024-05-15 20:15:48 -0300

    #15 Implementação chamada vídeo via webRTC

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,7 +1,24 @@
 <template>
   <v-main>
-    <v-container class="fill-height">
-      <div class="list-container fill-height">
+    <v-container class="fill-height pa-0">
+      <div v-if="store.getVideoCallStatus" class="video-container">
+        <v-col style="background-color: black;">
+          <v-progress-circular
+            v-if="!userVideoLoaded"
+            class="ma-8"
+            color="primary"
+            indeterminate
+            :size="68"
+            :width="6"
+          />
+          <div id="videos">
+          </div>
+          <v-btn v-if="userVideoLoaded" @click="hangUp" icon="mdi-phone-hangup" density="default" color="red" class="ma-3">
+          </v-btn>
+        </v-col>
+      </div>
+      <div class="list-container ma-3">
+        
         <v-text-field v-if="selectedItem" class="inputBox" label="Type a message" single-line hide-details
           @keyup.enter="sendNewMessage" rounded dense solo append-icon="" v-model="message"></v-text-field>
         <div v-for="(chatMessage, i) in store.getMessages(selectedItem).slice().reverse()" :key="chatMessage">
@@ -29,17 +46,32 @@
 
 <script setup>
 
-import { useAppStore } from '../../store/app';
-import { ref } from 'vue'
-import { defineProps, reactive, toRefs } from "vue";
+import { useAppStore } from '@/store/app';
+import { ref, watch, onMounted, onBeforeUnmount  } from 'vue'
+import { defineProps, toRefs } from "vue";
+import { watchRoom, joinRoom, saveUserSignal, getConfiguration, saveReadyUser, STATUS, closeConnection } from '@/scripts/signalingAPI';
+import { hangUpCurrentCall } from '@/scripts/callAPI';
 
 const props = defineProps({
   selectedItem: String,
 })
 
+onMounted(() => {
+  window.addEventListener('beforeunload', handleBeforeUnload);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('beforeunload', handleBeforeUnload);
+})
+
 const { selectedItem } = toRefs(props);
 
+let alreadyConnected = false;
+let localStream = {}
+let peers = {}
+
 const message = ref();
+const userVideoLoaded = ref(false);
 
 function sendNewMessage() {
   if (!message.value) return
@@ -48,6 +80,245 @@ function sendNewMessage() {
 }
 
 const store = useAppStore()
+
+const handleBeforeUnload = (event) => {
+  // Encerrar a conexão peer
+  const peersList = Object.keys(peers)
+  if (peersList.length > 0) {
+    for (let i = 0; i < peersList.length; i++) {
+      peers[peersList[i]].destroy()
+    }
+    closeConnection(store.incommingCallInfo.roomId, {signal: null, participant: store.currentUser.uid})
+  }
+  };
+
+watch(
+  () => store.isInCall,
+  (status) => {
+    if (status) {
+      // the user that was accepted the call are the initiator
+      if (store.incommingCallInfo.callerUID != store.currentUser.uid) {
+        navigator.mediaDevices.getUserMedia({ 
+        audio: true,
+        video: {
+          width: '480',
+          height: '360'
+        } 
+        }).then(async stream => {
+          localStream = stream
+          console.log('successfuly received local stream: ')
+          await saveReadyUser({id: store.incommingCallInfo.roomId})
+          watchAsInitiator();
+        }).catch (err => {
+          console.error('error occurred when triyng to get an access to local stream')
+          console.error(err)
+        })
+      } else {
+        watchCallAsNotInitator();
+        navigator.mediaDevices.getUserMedia({ 
+        audio: true,
+        video: {
+          width: '480',
+          height: '360'
+        } 
+        }).then(stream => {
+          console.log('successfuly received local stream: ')
+          localStream = stream
+        }).catch (err => {
+          console.error('error occurred when triyng to get an access to local stream')
+          console.error(err)
+        })
+      }
+    }
+  },
+  { immediate: true }
+);
+
+
+function watchCallAsNotInitator () {
+  watchRoom(store.incommingCallInfo.roomId, async (updatedRoom) => {
+    if (updatedRoom.currentState === STATUS.USER_READY_FOR_CONNECTION ) {
+      const readyUsers = JSON.parse(updatedRoom.readyUsers);
+      const readyUser = readyUsers[readyUsers.length - 1]
+      if (!peers.hasOwnProperty(readyUser) && readyUser !== store.currentUser.uid && !peers[readyUser]) {
+        console.log(`[WATCH ROOM]: New user added`)
+        const configuration = getConfiguration()
+
+        peers[readyUser] = new window.SimplePeer({
+          initiator: false,
+          config: configuration,
+          stream: localStream,
+          channelName: "messenger"
+        })
+        peers[readyUser].on('signal', (data) => {
+          const signalData = {
+            signal: data,
+            participant: store.currentUser.uid
+          }
+          saveUserSignal(updatedRoom, signalData)
+        })
+      
+        peers[readyUser].on('data', (data) => {
+          console.log("data", data)
+        })
+      
+        peers[readyUser].on('stream', (stream) => {
+          addStream(stream, readyUser)
+          userVideoLoaded.value = true;
+        })
+
+        peers[readyUser].on('close', (data) => {
+          console.log('close: ', data)
+        })
+
+        peers[readyUser].on('error', (data) => {
+          console.log('error', data)
+        })
+        await saveReadyUser(updatedRoom)
+      }
+    } else if (updatedRoom.currentState === STATUS.STARTING_CONNECTION) {
+      const signal = (updatedRoom.signals)
+      peers[signal.participant].signal(signal.signal)
+      alreadyConnected=true
+      if (signal.type === 'answer') {
+        alreadyConnected = true
+      }
+    } else if (updatedRoom.currentState === STATUS.CLOSING_CONNECTION) {
+        const signal = updatedRoom.signals
+        hangUp();
+        if (peers[signal.participant]) {
+          removeStream(signal.participant)
+          peers[signal.participant].destroy()
+          delete peers[signal.participant]
+        }
+      }
+  })
+}
+
+function watchAsInitiator() {
+  watchRoom(store.incommingCallInfo.roomId, async (updatedRoom) => {
+    if (updatedRoom.currentState === STATUS.USER_READY_FOR_CONNECTION ) {
+
+      const readyUsers = JSON.parse(updatedRoom.readyUsers);
+      const readyUser = readyUsers[readyUsers.length - 1]
+      if (!peers.hasOwnProperty(readyUser) && readyUser !== store.currentUser.uid && !peers[readyUser]) {
+        console.log(`[WATCH ROOM]: A user is ready for connection`, readyUser)
+        const configuration = getConfiguration()
+
+        peers[readyUser] = new window.SimplePeer({
+          initiator: !alreadyConnected,
+          config: configuration,
+          stream: localStream,
+          channelName: "messenger"
+        })
+        peers[readyUser].on('signal', (data) => {
+          // webRTC offer, webRTC Answer (SDP information), ice candidates
+          const signalData = {
+            signal: data,
+            participant: store.currentUser.uid
+          }
+          saveUserSignal(updatedRoom, signalData)
+        })
+      
+        peers[readyUser].on('data', (data) => {
+          console.log("data", data)
+        })
+
+        peers[readyUser].on('close', (data) => {
+          console.log('close: ', data)
+        })
+
+        peers[readyUser].on('error', (data) => {
+          console.log('error', data)
+        })
+        peers[readyUser].on('stream', (stream) => {
+          console.log('recebi a stream');
+          addStream(stream, readyUser)
+          userVideoLoaded.value = true;
+        })
+        await saveReadyUser({id: store.incommingCallInfo.roomId})
+      }
+    } else if (updatedRoom.currentState === STATUS.STARTING_CONNECTION) {
+      const signal = (updatedRoom.signals)
+      peers[signal.participant].signal(signal.signal)
+      if (signal.signal.type === 'answer') {
+        alreadyConnected = true
+        currentRoom = room.id
+        await joinRoom(room)
+      }
+
+    } else if (updatedRoom.currentState === STATUS.CLOSING_CONNECTION) {
+      hangUp();
+      const signal = updatedRoom.signals
+      if (peers[signal.participant]) {
+        removeStream(signal.participant)
+        peers[signal.participant].destroy()
+        delete peers[signal.participant]
+      }
+    }
+  })
+}
+
+const showLocalVideoPreview = (stream) => {
+  const videoContainer = document.getElementById("videos")
+  const videoElement = document.createElement('video')
+  videoElement.setAttribute('controls', true);
+  videoElement.autoplay = true
+  videoElement.muted = true
+  videoElement.style.padding = "5px";
+  // videoElement.style.width = "50%";
+  videoElement.style.height = "80%";
+  videoElement.srcObject = stream
+
+  videoElement.onloadedmetadata = () => {
+    videoElement.play()
+  }
+  videoContainer.appendChild(videoElement)
+}
+
+const addStream = (stream, userId) => {
+  const videoContainer = document.getElementById("videos")
+  const videoElement = document.createElement('video')
+  videoElement.setAttribute('controls', true);
+  videoElement.autoplay = true
+  videoElement.muted = true
+  videoElement.srcObject = stream
+  videoElement.id = `${userId}-video`
+
+  videoElement.onloadedmetadata = () => {
+    videoElement.play()
+  }
+  videoContainer.appendChild(videoElement)
+}
+
+const removeStream = (userId) => {
+  try {
+    const videoContainer = document.getElementById("videos")
+    const videoElement = document.getElementById(`${userId}-video`)
+    const tracks = videoElement.srcObject.getTracks()
+    tracks.forEach(t => t.stop())
+    videoElement.srcObject = null
+    videoContainer.removeChild(videoElement)
+  } catch (e) {
+    console.error("Error removing stream ", e)
+  }
+}
+function freeCam() {
+  if (localStream) {
+    localStream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+}
+function hangUp() {
+  handleBeforeUnload();
+  freeCam();
+  store.setCallInactive();
+  userVideoLoaded.value = false;
+  alreadyConnected = false;
+  peers = {};
+  hangUpCurrentCall();
+}
 
 </script>
 
@@ -74,5 +345,27 @@ span {
 .list-container {
   display: flex;
   flex-direction: column-reverse;
+  align-self: end;
+  margin-bottom: 5px;
+}
+
+.video-container {
+  display: flex;
+  width: 100%;
+  align-self: start;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  background-color: black;
+  height: 50%;
+}
+
+#videos {
+  display: flex;
+  align-self: start;
+  width: 100%;
+  background-color: black;
+  align-items: center;
+  justify-content: center
 }
 </style>
---
+++ src/scripts/signalingAPI.js
@@ -0,0 +1,162 @@
+import { ref, set, onValue, update, get, child } from 'firebase/database';
+import { db } from '@/firebase';
+import { v4 as uuidv4 } from 'uuid';
+import { useAppStore } from '@/store/app'
+import { fetchTURNCredential, getTurnIceServers } from '../turn'
+
+export const STATUS = Object.freeze({
+  CREATING_ROOM: 'creating-room',
+  JOINING_ROOM: 'joining-room',
+  USER_READY_FOR_CONNECTION: 'user-ready-for-connection',
+  STARTING_CONNECTION: 'starting-connection',
+  CONNECTED_USER: 'connected-user',
+  CLOSING_CONNECTION: 'closing-connection'
+});
+
+const appStore = useAppStore()
+fetchTURNCredential();
+
+export const handleBeforeUnload = (peers) => {
+  if (!peers) return;
+  for (const peer of peers) {
+    peer.destroy();
+  }
+};
+
+export const getConfiguration = () => {
+  const turnIceServers = getTurnIceServers()
+  if (turnIceServers) {
+    console.log('TURN server credentials fetched')
+    return {
+      icServers: [
+        {
+          urls: 'stun:stun.l.google.com:19302'
+        },
+        ...turnIceServers
+      ]
+    }
+  } else {
+    console.warn('Using only STUN server')
+    return {
+      icServers: [
+        {
+          urls: 'stun:stun.l.google.com:19302'
+        }
+      ]
+    }
+  }
+}
+
+export const createRoom = async (rID) => {
+  try {
+    const roomId = rID || uuidv4()
+    const userRef = ref(db, `room/${roomId}`)
+    const userData = {
+      name: rID,
+      participants: JSON.stringify([appStore.currentUser.uid]),
+      id: roomId,
+      readyUsers: JSON.stringify([]),
+      signals: JSON.stringify([]),
+      currentState: STATUS.CREATING_ROOM,
+      targets: '*'
+    };
+
+    await set(userRef, userData)
+    return userData
+  } catch (error) {
+    console.error('Failed to write data: ', error);
+    return {}
+  }
+}
+
+export const watchRoom = (roomId, updatedRoom) => {
+  const dataRef = ref(db, `room/${roomId}`)
+  onValue(dataRef, (snapshot) => {
+    const room = snapshot.val()
+    if (room.targets === '*' || room.targets.includes(appStore.currentUser.uid)) {
+      console.log(`Room ${roomId} updated`, room)
+      updatedRoom(room)
+    }
+  })
+}
+
+export const saveUserSignal = async (room, signal) => {
+  const roomRef = ref(db, `room/${room.id}`)
+  const readyUsers = JSON.parse(room.readyUsers)
+  setTimeout(async () => {
+    await update(roomRef, {
+      signals: signal,
+      currentState: STATUS.STARTING_CONNECTION,
+      targets: JSON.stringify(readyUsers.filter((p) => p !== appStore.currentUser.uid))
+    })
+  }, 500)
+}
+
+async function getRoomById(id) {
+  const dbRef = ref(db);
+  let room = null;
+  try {
+    room = await get(child(dbRef, `room/${id}`));
+    if (room.exists()) {
+      return room.val();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error getting collection data:', error);
+    return null;
+  }
+
+}
+
+export const saveReadyUser = async (room) => {
+  const roomRef = ref(db, `room/${room.id}`)
+  const roomData = await getRoomById(room.id);
+  let readyVector = JSON.parse(roomData.readyUsers)
+  readyVector.push(appStore.currentUser.uid)
+  setTimeout(async () => {
+    await update(roomRef, {
+      readyUsers: JSON.stringify(readyVector),
+      currentState: STATUS.USER_READY_FOR_CONNECTION,
+      targets: '*'
+    })
+  }, 500)
+}
+
+export const getAndWatchRooms = (updateRooms) => {
+  const dataRef = ref(db, 'room')
+    onValue(dataRef, (snapshot) => {
+      const rooms = snapshot.val()
+        updateRooms(rooms);
+    });
+}
+
+export const joinRoom = async (room) => {
+  try {
+    const roomRef = ref(db, `room/${room.id}`)
+    let participants = JSON.parse(room.participants)
+    participants.push(appStore.currentUser.uid)
+    setTimeout(async () => {
+      await update(roomRef, {
+        participants: JSON.stringify(participants),
+        currentState: STATUS.CONNECTED_USER,
+        readyUsers: "[]",
+        targets: JSON.stringify(participants.filter((p) => p !== appStore.currentUser.uid))
+      })
+    }, 500)
+
+    return true
+  } catch (error) {
+    console.log("[ERROR ON UPDATE ROOM]: ", error)
+    return false
+  }
+}
+
+export const closeConnection = async (room, signal) => {
+  const roomRef = ref(db, `room/${room}`)
+  await update(roomRef, {
+    currentState: STATUS.CLOSING_CONNECTION,
+    targets: '*',
+    signals: signal
+  })
+}
