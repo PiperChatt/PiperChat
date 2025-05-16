11762d745930cfba2a5b47fbb7cd0bf30528c50c
GedorN <gedor.silvaneto@gmail.com>
2025-05-16 08:49:13 -0300

    #35: Correção ratio da janela de video chamada

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -616,8 +616,8 @@ span {
 }
 
 .videos-container {
-  width: 100%;
-  height: 100%;
+  /* width: 100%; */
+  /* height: 100%; */
   display: flex;
   align-items: center;
   justify-content: center;
